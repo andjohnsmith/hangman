ee1291c7cde5a615e68f4bf3b9d80bdabd343700
Andrew Smith <smithandrewj@outlook.com>
2020-05-07 20:26:18 -0700

    start to restructure

---
+++ models/Game.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema(
+  {
+    view: { type: String, required: true },
+    answer: { type: String, required: true },
+    turns: { type: Number, required: true },
+    guesses: { type: String },
+  },
+  { timestamps: true },
+);
+
+module.exports = Game = mongoose.model('Game', GameSchema);
